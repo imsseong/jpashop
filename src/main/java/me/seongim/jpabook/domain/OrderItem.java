611d054ca46a86286796be4e7ac62e987788f38f
imsseong <hsung325@gmail.com>
2023-02-20 22:21:56 +0900

    주문, 주문상품 엔티티 비즈니스 로직 추가

--- src/main/java/me/seongim/jpabook/domain/OrderItem.java
+++ src/main/java/me/seongim/jpabook/domain/OrderItem.java
@@ -6,6 +6,8 @@ import me.seongim.jpabook.domain.item.Item;
 
 import javax.persistence.*;
 
+import java.time.LocalDateTime;
+
 import static javax.persistence.FetchType.*;
 
 @Entity
@@ -26,4 +28,25 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count; //주문 수향
+
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removetock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count); //재고 수량 원상복구
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
