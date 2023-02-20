f1a99848a020d847dfdb5c599a7a5c0b38e2e9ce
imsseong <hsung325@gmail.com>
2023-02-20 21:49:56 +0900

    상품 엔티티 stock 증감 비즈니스 로직 추가

--- src/main/java/me/seongim/jpabook/domain/item/Item.java
+++ src/main/java/me/seongim/jpabook/domain/item/Item.java
@@ -3,6 +3,7 @@ package me.seongim.jpabook.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 import me.seongim.jpabook.domain.Category;
+import me.seongim.jpabook.exception.NotEnoughStockException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -25,4 +26,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    /**
+     * stock 증가
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     * @param quantity
+     */
+    public void removetock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
